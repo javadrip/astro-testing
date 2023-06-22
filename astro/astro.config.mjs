b82f4f5e3a0624ba329870dd423127c7161ab808
Vinleon Ang <javadripdev@gmail.com>
2023-06-22 12:54:54 +0800

    Install image

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import sanity from "astro-sanity";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "vqhz93qr",
-      dataset: "production",
-      apiVersion: "2023-06-06",
-      useCdn: true,
-    }),
-  ],
+  integrations: [sanity({
+    projectId: "vqhz93qr",
+    dataset: "production",
+    apiVersion: "2023-06-06",
+    useCdn: true
+  }), image()]
 });
