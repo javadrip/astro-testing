0f62865702ed1e40754068e034dc52621054783c
Vinleon Ang <javadripdev@gmail.com>
2023-06-25 21:17:47 +0800

    Move schema files

---
+++ astro/sanity/schemas/post.ts
@@ -0,0 +1,82 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'Ideally between 15 and 70 characters',
+      validation: (Rule) => [
+        Rule.required().min(15).warning('The title is probably too short.'),
+        Rule.max(70).warning('Shorter titles are usually better.'),
+      ],
+    }),
+    defineField({
+      name: 'postMetaDescription',
+      title: 'Meta description',
+      type: 'text',
+      description: 'Ideally between 70 and 160 characters',
+      validation: (Rule) => [
+        Rule.required().min(70).warning('Add in more details.'),
+        Rule.max(160).warning('Content beyond 160 characters might be truncated.'),
+      ],
+    }),
+    defineField({
+      name: 'postSlug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'},
+    }),
+    defineField({
+      name: 'featuredImage',
+      title: 'Featured image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        },
+      ],
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
