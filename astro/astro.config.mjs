a6edd225658546bab26a6f250826c55844af1475
Vinleon Ang <javadripdev@gmail.com>
2023-06-25 22:50:24 +0800

    Add react

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import sanity from "astro-sanity";
-
 import image from "@astrojs/image";
+import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +12,9 @@ export default defineConfig({
     dataset: "production",
     apiVersion: "2023-06-06",
     useCdn: true
-  }), image()]
+  }), image(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
