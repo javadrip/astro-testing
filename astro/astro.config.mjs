c56bf165f68a672a1658a2b34f7a3707dec0a6ea
Vinleon Ang <javadripdev@gmail.com>
2023-06-26 14:33:48 +0800

    Remove node

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,18 +3,16 @@ import sanity from "astro-sanity";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: "vqhz93qr",
-    dataset: "production",
-    apiVersion: "2023-06-06",
-    useCdn: true
-  }), image(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  integrations: [
+    sanity({
+      projectId: "vqhz93qr",
+      dataset: "production",
+      apiVersion: "2023-06-06",
+      useCdn: true,
+    }),
+    image(),
+    react(),
+  ],
 });
